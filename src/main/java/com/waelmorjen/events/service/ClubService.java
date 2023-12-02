2dc52d39667eb9ab37d7782faba024249e92020f
Wael-Morjen <waelmorjen@gmail.com>
2023-12-02 18:58:41 +0100

    create and save club to DB

--- src/main/java/com/waelmorjen/events/service/ClubService.java
+++ src/main/java/com/waelmorjen/events/service/ClubService.java
@@ -1,9 +1,11 @@
 package com.waelmorjen.events.service;
 
 import com.waelmorjen.events.dto.ClubDto;
+import com.waelmorjen.events.models.Club;
 
 import java.util.List;
 
 public interface ClubService {
     List<ClubDto> findAllClubs();
+    Club saveClub(Club club);
 }
