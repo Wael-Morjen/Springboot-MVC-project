26749b3ee8159f07f32826b3e4087f8515896f2c
Wael-Morjen <waelmorjen@gmail.com>
2023-12-02 18:27:28 +0100

    Club service implementation

---
+++ src/main/java/com/waelmorjen/events/service/impl/ClubServiceImpl.java
@@ -0,0 +1,37 @@
+package com.waelmorjen.events.service.impl;
+
+import com.waelmorjen.events.dto.ClubDto;
+import com.waelmorjen.events.models.Club;
+import com.waelmorjen.events.repository.ClubRepository;
+import com.waelmorjen.events.service.ClubService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ClubServiceImpl implements ClubService {
+
+    private ClubRepository clubRepository;
+
+    public ClubServiceImpl(ClubRepository clubRepository) {
+        this.clubRepository = clubRepository;
+    }
+    @Override
+    public List<ClubDto> findAllClubs() {
+        List<Club> clubs = clubRepository.findAll();
+        return clubs.stream().map(club -> mapToClubDto(club)).collect(Collectors.toList());
+    }
+
+    private ClubDto mapToClubDto(Club club) {
+        ClubDto clubDto = ClubDto.builder()
+                .id(club.getId())
+                .title(club.getTitle())
+                .photoUrl(club.getPhotoUrl())
+                .content(club.getContent())
+                .createdOn(club.getCreatedOn())
+                .updatedOn(club.getUpdatedOn())
+                .build();
+        return clubDto;
+    }
+}
