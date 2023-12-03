5490d49a1348ed6729f0a3d6150f198c2bcc98e5
Wael-Morjen <waelmorjen@gmail.com>
2023-12-03 20:27:05 +0100

    Validation

--- src/main/java/com/waelmorjen/events/dto/ClubDto.java
+++ src/main/java/com/waelmorjen/events/dto/ClubDto.java
@@ -1,5 +1,6 @@
 package com.waelmorjen.events.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,8 +10,11 @@ import java.time.LocalDateTime;
 @Builder
 public class ClubDto {
     private Long id;
+    @NotEmpty(message = "Club Title should not be empty")
     private String title;
+    @NotEmpty(message = "A photo url should be provided")
     private String photoUrl;
+    @NotEmpty(message = "Content should not be empty")
     private String content;
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
