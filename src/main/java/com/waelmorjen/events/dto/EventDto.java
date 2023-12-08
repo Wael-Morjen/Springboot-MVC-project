b941fd12f13d74280ac0a64fadc40dd866b5afc9
Wael-Morjen <waelmorjen@gmail.com>
2023-12-08 15:34:21 +0100

    Event DTO

---
+++ src/main/java/com/waelmorjen/events/dto/EventDto.java
@@ -0,0 +1,27 @@
+package com.waelmorjen.events.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventDto {
+    private Long id;
+    private String name;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String type;
+    private String photoUrl;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+}
