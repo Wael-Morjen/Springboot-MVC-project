b43e9d38e07b6c85ad67346a43f8f1a36cc6b902
Wael-Morjen <waelmorjen@gmail.com>
2023-12-08 16:41:01 +0100

    Club mapper

---
+++ src/main/java/com/waelmorjen/events/mapper/ClubMapper.java
@@ -0,0 +1,30 @@
+package com.waelmorjen.events.mapper;
+
+import com.waelmorjen.events.dto.ClubDto;
+import com.waelmorjen.events.models.Club;
+
+public class ClubMapper {
+    public static Club mapToClub(ClubDto club) {
+        Club clubDto = Club.builder()
+                .id(club.getId())
+                .title(club.getTitle())
+                .photoUrl(club.getPhotoUrl())
+                .content(club.getContent())
+                .createdOn(club.getCreatedOn())
+                .updatedOn(club.getUpdatedOn())
+                .build();
+        return clubDto;
+    }
+
+    public static ClubDto mapToClubDto(Club club) {
+        ClubDto clubDto = ClubDto.builder()
+                .id(club.getId())
+                .title(club.getTitle())
+                .photoUrl(club.getPhotoUrl())
+                .content(club.getContent())
+                .createdOn(club.getCreatedOn())
+                .updatedOn(club.getUpdatedOn())
+                .build();
+        return clubDto;
+    }
+}
