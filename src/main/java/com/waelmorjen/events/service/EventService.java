e2088917c48df9c60a08a77da2cfba07408ad412
Wael-Morjen <waelmorjen@gmail.com>
2023-12-08 18:07:52 +0100

    findAllEvents method

--- src/main/java/com/waelmorjen/events/service/EventService.java
+++ src/main/java/com/waelmorjen/events/service/EventService.java
@@ -2,6 +2,10 @@ package com.waelmorjen.events.service;
 
 import com.waelmorjen.events.dto.EventDto;
 
+import java.util.List;
+
 public interface EventService {
     void createEvent(Long clubId, EventDto eventDto);
+
+    List<EventDto> findAllEvents();
 }
