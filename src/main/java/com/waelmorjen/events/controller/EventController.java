6b5db3807c15908924bf1197ce83597bf341444f
Wael-Morjen <waelmorjen@gmail.com>
2023-12-08 16:30:45 +0100

    POST Request to create event

---
+++ src/main/java/com/waelmorjen/events/controller/EventController.java
@@ -0,0 +1,41 @@
+package com.waelmorjen.events.controller;
+
+import com.waelmorjen.events.dto.EventDto;
+import com.waelmorjen.events.models.Event;
+import com.waelmorjen.events.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class EventController {
+
+    private EventService eventService;
+
+    @Autowired
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping("/events/{clubId}/new")
+    public String createEventForm(@PathVariable("clubId") Long clubId, Model model) {
+        Event event = new Event();
+        model.addAttribute("clubId", clubId);
+        model.addAttribute("event", event);
+        return "events-create";
+    }
+
+    @PostMapping("/events/{clubId}")
+    public String createEvent(
+            @PathVariable("clubId") Long clubId,
+            @ModelAttribute("event") EventDto eventDto,
+            Model model
+    ) {
+        eventService.createEvent(clubId, eventDto);
+        return "redirect:/clubs/" + clubId;
+    }
+}
