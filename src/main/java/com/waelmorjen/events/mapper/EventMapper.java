56ad3c44a3fca7142fbc70c33b368fadf42665d7
Wael-Morjen <waelmorjen@gmail.com>
2023-12-08 16:41:07 +0100

    Event mapper

---
+++ src/main/java/com/waelmorjen/events/mapper/EventMapper.java
@@ -0,0 +1,19 @@
+package com.waelmorjen.events.mapper;
+
+import com.waelmorjen.events.dto.EventDto;
+import com.waelmorjen.events.models.Event;
+
+public class EventMapper {
+    public static Event mapToEvent(EventDto eventDto) {
+        return Event.builder()
+                .id(eventDto.getId())
+                .name(eventDto.getName())
+                .startTime(eventDto.getStartTime())
+                .endTime(eventDto.getEndTime())
+                .type(eventDto.getType())
+                .photoUrl(eventDto.getPhotoUrl())
+                .createdOn(eventDto.getCreatedOn())
+                .updatedOn(eventDto.getUpdatedOn())
+                .build();
+    }
+}
