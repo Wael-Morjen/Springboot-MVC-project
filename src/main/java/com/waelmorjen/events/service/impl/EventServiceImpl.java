5bd4323146bc19b1786004194e956add33df4bf6
Wael-Morjen <waelmorjen@gmail.com>
2023-12-08 15:35:20 +0100

    Event Service createEvent implementation

---
+++ src/main/java/com/waelmorjen/events/service/impl/EventServiceImpl.java
@@ -0,0 +1,41 @@
+package com.waelmorjen.events.service.impl;
+
+import com.waelmorjen.events.dto.EventDto;
+import com.waelmorjen.events.models.Club;
+import com.waelmorjen.events.models.Event;
+import com.waelmorjen.events.repository.ClubRepository;
+import com.waelmorjen.events.repository.EventRepository;
+import com.waelmorjen.events.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EventServiceImpl implements EventService {
+
+    private EventRepository eventRepository;
+    private ClubRepository clubRepository;
+
+    public EventServiceImpl(EventRepository eventRepository, ClubRepository clubRepository) {
+        this.eventRepository = eventRepository;
+        this.clubRepository = clubRepository;
+    }
+
+    @Override
+    public void createEvent(Long clubId, EventDto eventDto) {
+        Club club = clubRepository.findById(clubId).get();
+        Event event =mapToEvent(eventDto);
+        event.setClub(club);
+        eventRepository.save(event);
+    }
+
+    private Event mapToEvent(EventDto eventDto) {
+        return Event.builder()
+                .id(eventDto.getId())
+                .name(eventDto.getName())
+                .startTime(eventDto.getStartTime())
+                .endTime(eventDto.getEndTime())
+                .type(eventDto.getType())
+                .photoUrl(eventDto.getPhotoUrl())
+                .createdOn(eventDto.getCreatedOn())
+                .updatedOn(eventDto.getUpdatedOn())
+                .build();
+    }
+}
