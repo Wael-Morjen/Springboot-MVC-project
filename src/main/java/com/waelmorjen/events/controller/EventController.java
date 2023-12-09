827bffbf9846243b6efe6711952bf84e2ffc4c88
Wael-Morjen <waelmorjen@gmail.com>
2023-12-09 16:10:27 +0100

    GET/POST request to update event

--- src/main/java/com/waelmorjen/events/controller/EventController.java
+++ src/main/java/com/waelmorjen/events/controller/EventController.java
@@ -1,11 +1,14 @@
 package com.waelmorjen.events.controller;
 
+import com.waelmorjen.events.dto.ClubDto;
 import com.waelmorjen.events.dto.EventDto;
 import com.waelmorjen.events.models.Event;
 import com.waelmorjen.events.service.EventService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,9 +52,39 @@ public class EventController {
     public String createEvent(
             @PathVariable("clubId") Long clubId,
             @ModelAttribute("event") EventDto eventDto,
+            BindingResult result,
             Model model
     ) {
+        if (result.hasErrors()) {
+            model.addAttribute("event", eventDto);
+            return "clubs-create";
+        }
         eventService.createEvent(clubId, eventDto);
         return "redirect:/clubs/" + clubId;
+    }
+
+    @GetMapping("/events/{eventId}/edit")
+    public String editEventForm(@PathVariable("eventId") Long eventId, Model model) {
+        EventDto event = eventService.findByEventId(eventId);
+        model.addAttribute("event", event);
+        return "events-edit";
+    }
+
+    @PostMapping("/events/{eventId}/edit")
+    public String updateEvent(
+            @PathVariable("eventId") Long eventId,
+            @Valid @ModelAttribute("event") EventDto event,
+            BindingResult result,
+            Model model
+    ) {
+        if (result.hasErrors()) {
+            model.addAttribute("event", event);
+            return "events-edit";
+        }
+        EventDto eventDto = eventService.findByEventId(eventId);
+        event.setId(eventId);
+        event.setClub(eventDto.getClub());
+        eventService.updateEvent(event);
+        return "redirect:/events";
     }
 }
