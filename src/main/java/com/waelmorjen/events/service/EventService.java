e88db783761822c1fdf8971c516235395000a123
Wael-Morjen <waelmorjen@gmail.com>
2023-12-09 16:09:06 +0100

    update event method

--- src/main/java/com/waelmorjen/events/service/EventService.java
+++ src/main/java/com/waelmorjen/events/service/EventService.java
@@ -5,9 +5,12 @@ import com.waelmorjen.events.dto.EventDto;
 import java.util.List;
 
 public interface EventService {
+
     void createEvent(Long clubId, EventDto eventDto);
 
     List<EventDto> findAllEvents();
 
     EventDto findByEventId(Long eventId);
+
+    void updateEvent(EventDto eventDto);
 }
