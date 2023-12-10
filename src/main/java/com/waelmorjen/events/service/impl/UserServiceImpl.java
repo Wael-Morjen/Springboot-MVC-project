2c3cb64d166fcce29628ac0050e031f201838cc4
Wael-Morjen <waelmorjen@gmail.com>
2023-12-10 16:22:16 +0100

    saveUser method impl

---
+++ src/main/java/com/waelmorjen/events/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.waelmorjen.events.service.impl;
+
+import com.waelmorjen.events.dto.RegistrationDto;
+import com.waelmorjen.events.models.Role;
+import com.waelmorjen.events.models.UserEntity;
+import com.waelmorjen.events.repository.RoleRepository;
+import com.waelmorjen.events.repository.UserRepository;
+import com.waelmorjen.events.service.UserService;
+
+import java.util.Arrays;
+
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void saveUser(RegistrationDto registrationDto) {
+        UserEntity user = new UserEntity();
+        user.setUsername(registrationDto.getUsername());
+        user.setEmail(registrationDto.getEmail());
+        user.setPassword(registrationDto.getPassword());
+        Role role = roleRepository.findByName("USER");
+        user.setRoles(Arrays.asList(role));
+        userRepository.save(user);
+    }
+}
