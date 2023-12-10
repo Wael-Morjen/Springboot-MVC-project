1b63c0d24e4f706f750e93e94b1aa2ef7168e95f
Wael-Morjen <waelmorjen@gmail.com>
2023-12-10 15:13:16 +0100

    Extending JpaRepository

--- src/main/java/com/waelmorjen/events/repository/RoleRepository.java
+++ src/main/java/com/waelmorjen/events/repository/RoleRepository.java
@@ -1,8 +1,9 @@
 package com.waelmorjen.events.repository;
 
 import com.waelmorjen.events.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository {
+public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
 
 }
--- src/main/java/com/waelmorjen/events/repository/UserRepository.java
+++ src/main/java/com/waelmorjen/events/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.waelmorjen.events.repository;
 
 import com.waelmorjen.events.models.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);
     UserEntity findByUsername(String username);
 }
