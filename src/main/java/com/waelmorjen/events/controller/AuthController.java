4a9771cdb7f70a8708dd4521b0d6cd0b2bed76e1
Wael-Morjen <waelmorjen@gmail.com>
2023-12-10 17:59:39 +0100

    authController login/register get request

---
+++ src/main/java/com/waelmorjen/events/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.waelmorjen.events.controller;
+
+import com.waelmorjen.events.dto.RegistrationDto;
+import com.waelmorjen.events.models.UserEntity;
+import com.waelmorjen.events.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    private UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/login")
+    public String loginPage(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterForm(Model model) {
+        RegistrationDto user = new RegistrationDto();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+
+}
