b8e63d22b28190098b47653e96f9de602a0e43cf
Wael-Morjen <waelmorjen@gmail.com>
2023-12-10 18:00:01 +0100

    register and save the user

--- src/main/java/com/waelmorjen/events/controller/AuthController.java
+++ src/main/java/com/waelmorjen/events/controller/AuthController.java
@@ -32,5 +32,22 @@ public class AuthController {
         return "register";
     }
 
-
+    @PostMapping("/register/save")
+    public String register(@Valid @ModelAttribute("user")RegistrationDto user,
+                           BindingResult result, Model model) {
+        UserEntity existingUserEmail = userService.findByEmail(user.getEmail());
+        if(existingUserEmail != null && existingUserEmail.getEmail() != null && !existingUserEmail.getEmail().isEmpty()) {
+            return "redirect:/register?fail";
+        }
+        UserEntity existingUserUsername = userService.findByUsername(user.getUsername());
+        if(existingUserUsername != null && existingUserUsername.getUsername() != null && !existingUserUsername.getUsername().isEmpty()) {
+            return "redirect:/register?fail";
+        }
+        if(result.hasErrors()) {
+            model.addAttribute("user", user);
+            return "register";
+        }
+        userService.saveUser(user);
+        return "redirect:/clubs?success";
+    }
 }
