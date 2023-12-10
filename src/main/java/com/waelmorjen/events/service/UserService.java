0294db87ed1982c26537e473760ad906c91eb88f
Wael-Morjen <waelmorjen@gmail.com>
2023-12-10 18:01:08 +0100

    findByEmail/findByUsername method

--- src/main/java/com/waelmorjen/events/service/UserService.java
+++ src/main/java/com/waelmorjen/events/service/UserService.java
@@ -1,7 +1,12 @@
 package com.waelmorjen.events.service;
 
 import com.waelmorjen.events.dto.RegistrationDto;
+import com.waelmorjen.events.models.UserEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserService {
     void saveUser(RegistrationDto registrationDto);
+    UserEntity findByEmail(String email);
+    UserEntity findByUsername(String username);
 }
