5af97f769b379e227bd3bc07ff0e9245a1f8b97a
Wael-Morjen <waelmorjen@gmail.com>
2023-12-11 19:04:32 +0100

    password encoder impl

--- src/main/java/com/waelmorjen/events/service/impl/UserServiceImpl.java
+++ src/main/java/com/waelmorjen/events/service/impl/UserServiceImpl.java
@@ -6,17 +6,24 @@ import com.waelmorjen.events.models.UserEntity;
 import com.waelmorjen.events.repository.RoleRepository;
 import com.waelmorjen.events.repository.UserRepository;
 import com.waelmorjen.events.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
     private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -24,7 +31,7 @@ public class UserServiceImpl implements UserService {
         UserEntity user = new UserEntity();
         user.setUsername(registrationDto.getUsername());
         user.setEmail(registrationDto.getEmail());
-        user.setPassword(registrationDto.getPassword());
+        user.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
         Role role = roleRepository.findByName("USER");
         user.setRoles(Arrays.asList(role));
         userRepository.save(user);
