dd776b0ac2c26ad32fb490f5f887f97b36c386bc
Wael-Morjen <waelmorjen@gmail.com>
2023-12-13 13:39:44 +0100

    minor change

--- src/main/java/com/waelmorjen/events/models/UserEntity.java
+++ src/main/java/com/waelmorjen/events/models/UserEntity.java
@@ -24,7 +24,7 @@ public class UserEntity {
     private String password;
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(
-            name = "user_roles",
+            name = "users_roles",
             joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
             inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")}
     )
